b5ae1e7da4c74510c708a2fb8b2411e54a850508
Rian Arip Nugraha <87195694+abahpolban@users.noreply.github.com>
2023-11-18 14:50:13 +0700

    Update analyzing international debt.sql

--- analyzing international debt.sql
+++ analyzing international debt.sql
@@ -1,13 +1,30 @@
-﻿-- Table: international_debt
+-- ----------------------------
+-- ANALYZING INTERNATIONAL DEBT
+-- ----------------------------
+USE international_debt;
 
-CREATE TABLE international_debt
-(
-  country_name character varying(50),
-  country_code character varying(50),
-  indicator_name text,
-  indicator_code text,
-  debt numeric
-);
+-- 1. What is the total amount of debt that is owed by the countries listed in the dataset?
+select 
+    country_name,
+    round(sum(debt),1) as total_debt
+from international_debt
+group by country_name;
 
--- Copy over data from CSV
-\copy international_debt FROM 'international_debt.csv' DELIMITER ',' CSV HEADER;
+-- 2. Which country owns the maximum amount of debt and what does that amount look like?
+select 
+    country_name as highest_country_debt,
+    round(sum(debt),1) as total_debt
+from international_debt
+group by country_name
+order by 2 desc
+limit 1;
+
+-- 3. What is the average amount of debt owed by countries across different debt indicators?
+select 
+    country_name, 
+    indicator_name,
+    indicator_code, 
+    round(avg(debt),1) as average_debt
+from international_debt
+group by country_name, indicator_code, indicator_name
+order by country_name;
